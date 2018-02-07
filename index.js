e532c25313e684a3889be2d67e25b9622aa8ee99
Preetish Chandra <15859928+ExpertSunny@users.noreply.github.com>
2018-02-07 17:05:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('pxEpgcDBM6d35gJyYFn7xz1ytn0DeEwt'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
